4ecb87fee5d277e81b0a863fad2237c5b922780d
Nikhil Wadhwani <nikhilwadhwani2111@gmail.com>
2026-08-18 16:02:01 +0530

    Time: 4 ms (98.14%), Space: 46.5 MB (65.18%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,31 +1,25 @@
 class Solution {
-    public String frequencySort(String s) 
-    {
-        int freq[]=new int[256];
-        for(int i=0;i<s.length();i++)
-        {
-            char p=s.charAt(i);
+    public String frequencySort(String s) {
+        int freq[] = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            char p = s.charAt(i);
             freq[p]++;
         }
 
-        StringBuilder ans=new StringBuilder();
-        while(ans.length()!=s.length())
-        {
-            int m=0;
-            int max=freq[0];
-            for(int i=1;i<256;i++)
-            {
-                if(max<freq[i])
-                {
-                    m=i;
-                    max=freq[i];
+        StringBuilder ans = new StringBuilder();
+        while (ans.length() != s.length()) {
+            int m = 0;
+            int max = freq[0];
+            for (int i = 1; i < 256; i++) {
+                if (max < freq[i]) {
+                    m = i;
+                    max = freq[i];
                 }
             }
-            for(int i=max;i>0;i--)
-            {
-                freq[m]--;
-                ans.append((char)m);
-            }
+            for (int i = 0; i < max; i++)
+                ans.append((char) m);
+
+            freq[m] = 0;
         }
         return ans.toString();
     }
